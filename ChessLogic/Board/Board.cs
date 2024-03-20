c80a4940470021b12cd903c1e7d0c5db92118861
AZURIIEAL <abinbinu678@gmail.com>
2024-03-20 06:13:35 +0530

    minor changes

---
+++ ChessLogic/Board/Board.cs
@@ -0,0 +1,20 @@
+﻿using ChessLogic.Movement;
+using ChessLogic.Pieces;
+
+namespace ChessLogic.Board
+{
+    public class Board
+    {
+        private readonly Piece[,] _pieces = new Piece[8, 8];
+        public Piece this[int row, int col]
+        {
+            get { return _pieces[row, col]; }
+            set { _pieces[row, col] = value; }
+        }
+        public Piece this[Position position]
+        {
+            get { return this[position.Row,position.Column]; }
+            set { this[position.Row,position.Column] = value;}
+        }
+    }
+}
