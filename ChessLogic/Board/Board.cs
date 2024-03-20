05e82289e4b457c99a12e855943776fe6dedaa2c
AZURIIEAL <abinbinu678@gmail.com>
2024-03-20 06:39:33 +0530

    board logic

--- ChessLogic/Board/Board.cs
+++ ChessLogic/Board/Board.cs
@@ -1,5 +1,6 @@
 ﻿using ChessLogic.Movement;
 using ChessLogic.Pieces;
+using Player = ChessLogic.Enums.Player;
 
 namespace ChessLogic.Board
 {
@@ -13,8 +14,40 @@ namespace ChessLogic.Board
         }
         public Piece this[Position position]
         {
-            get { return this[position.Row,position.Column]; }
-            set { this[position.Row,position.Column] = value;}
+            get { return this[position.Row, position.Column]; }
+            set { this[position.Row, position.Column] = value; }
+        }
+        public static Board Initial()
+        {
+            Board board = new();
+            board.AddStartPieces();
+            return board;
+        }
+
+        private void AddStartPieces()
+        {
+            Type[] blackPieces = { typeof(Rook), typeof(Knight), typeof(Bishop), typeof(Queen), typeof(King), typeof(Bishop), typeof(Knight), typeof(Rook) };
+            Type[] whitePieces = { typeof(Rook), typeof(Knight), typeof(Bishop), typeof(Queen), typeof(King), typeof(Bishop), typeof(Knight), typeof(Rook) };
+
+            for (int col = 0; col < 8; col++)
+            {
+                this[0, col] = (Piece?)Activator.CreateInstance(blackPieces[col], Player.Black) ?? throw new InvalidOperationException("Failed to create piece.");
+                this[1, col] = new Pawn(Player.Black);
+            }
+
+            for (int col = 0; col < 8; col++)
+            {
+                this[7, col] = (Piece?)Activator.CreateInstance(whitePieces[col], Player.White) ?? throw new InvalidOperationException("Failed to create piece."); ;
+                this[6, col] = new Pawn(Player.White);
+            }
+        }
+        public static bool IsInside(Position position)
+        {
+            return (position.Row >= 0 && position.Row < 8) && (position.Column >= 0 && position.Column < 8);
+        }
+        public bool IsEmpty(Position position)
+        {
+            return this[position] is null;
         }
     }
 }
